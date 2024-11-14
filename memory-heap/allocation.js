1cc5f9f3bd8592b461ec8b987380ae9b1439466c
mmkvdev <madhuv299@gmail.com>
2024-11-14 07:40:05 +0530

    concept/memory_allocation: Dynamic and Static Memory Allocation

---
+++ memory-heap/allocation.js
@@ -0,0 +1,17 @@
+// JS Engine allocates memory for this in the heap. The actual values are still primitive, which is why they're stored in the stack
+const person = {
+    name: 'Guts',
+    age: 27
+};
+
+// arrays are objects, so they're stored in the heap.
+const hobbies = ['photography', 'biking'];
+
+let falconWarriorName = 'Griffith'; // allocates memory for a string
+const age = 27; // allocates memory for a number
+
+falconWarriorName = 'Warrior'; // allocates memory for a new string
+const firstName = falconWarriorName.slice(0,4);
+
+console.log(firstName);
+
