da622c432344dd7e7fae5bf993e252d8550563bf
mmkvdev <madhuv299@gmail.com>
2024-11-24 21:10:46 +0530

    concept/hoisting: adding a classic example of hoisting for function declaration and expression

---
+++ hoisting/examples/example_1.js
@@ -0,0 +1,14 @@
+console.log(teddy);
+console.log(sing());
+console.log(sing2());
+
+var teddy = 'bear';
+
+// function expression
+var sing2 = function () {
+    console.log('uhh la la');
+}
+// function declaration
+function sing () {
+    console.log('ohh la la');
+}
