7ea4077dd43ab9398aec76287cc90fbb284e575e
mmkvdev <madhuv299@gmail.com>
2024-11-30 22:02:40 +0530

    concept/scope: Block Scope

---
+++ scope/block_scope.js
@@ -0,0 +1,13 @@
+for (var i = 1; i < 5; i++) {
+    console.log(i);
+}
+
+console.log('outer value of i:', i);
+
+// let example
+
+for (let j = 1; j < 5; j++) {
+    console.log(j);
+}
+
+console.log('outer value of letted i: ', j);
