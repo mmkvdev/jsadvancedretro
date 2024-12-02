7fbd98bfd9aba79d62df7f324adb77bcec6d78d4
mmkvdev <madhuv299@gmail.com>
2024-12-02 09:04:50 +0530

    concept/scope: block scope examples

--- scope/block_scope.js
+++ scope/block_scope.js
@@ -1,3 +1,4 @@
+// example 1
 for (var i = 1; i < 5; i++) {
     console.log(i);
 }
@@ -11,3 +12,26 @@ for (let j = 1; j < 5; j++) {
 }
 
 console.log('outer value of letted i: ', j);
+
+
+// example 2
+var blockScopeName = 'name';
+
+{
+    let blockScopeName = 'another name';
+}
+
+console.log(blockScopeName);
+
+
+// example 3
+function blockScopeExample () {
+    for (let i1 = 0; i1 < 5; i1++) {
+        console.log(i1);
+    }
+
+    console.log('final value of i: ', i1);
+    return i1;
+}
+
+blockScopeExample();
