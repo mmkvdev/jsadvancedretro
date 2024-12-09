432c5cea8a998e20cdcb358100ae477e80e38804
mmkvdev <madhuv299@gmail.com>
2024-12-09 08:51:09 +0530

    concept/this: updated info for this

--- this/explicit_binding/index.js
+++ this/explicit_binding/index.js
@@ -2,6 +2,7 @@ const explicitThisBinding = function () {
     console.log('this is a function where explicit binding of this happens');
 }
 
+// all functions when created have a property `call` that allows us to call the function
 explicitThisBinding(); // this is the shorthand
 explicitThisBinding.call(); // internally all functions use `call()` while invoking a function
 explicitThisBinding.apply(); // call and apply do the same thing at this point
