c3419048c2b531c1cb9e49e61392fe4c7366d909
mmkvdev <madhuv299@gmail.com>
2024-12-12 09:53:00 +0530

    concept/this: updating documentation for bind

--- this/explicit_binding/bind/index.js
+++ this/explicit_binding/bind/index.js
@@ -1,3 +1,5 @@
+// `bind` returns a new function with a certain context and parameters
+// usually used when we want to call a function at a later point in time with a certain context or a certain type of `this` keyword
 const bindExample = {
     name: 'bind example',
     testing () {
