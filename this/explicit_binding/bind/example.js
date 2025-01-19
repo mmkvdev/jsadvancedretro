c89d971da999f3fa6b8193df9defac0d53bcc0a8
Madhu Kiran Varma <28980183+mmkvdev@users.noreply.github.com>
2025-01-19 12:40:16 +0530

    concept/this: adding more concrete examples for bind method

--- this/explicit_binding/bind/example.js
+++ this/explicit_binding/bind/example.js
@@ -6,3 +6,24 @@ function logId () {
 }
 
 button.addEventListener('click', logId.bind(button));
+
+
+
+// using spread operator to send a variable number of arguments to the shared method of the contextual object
+const wizard = {
+  name: "test",
+  health: 50,
+  heal(...args) {
+    console.log({ args })
+    this.health += 100 + args[0]
+  },
+}
+
+const archer = {
+  name: "archer",
+  health: 30,
+}
+
+const testing = wizard.heal.bind(archer, 1, 2, 3, 4)
+
+console.log({ test: testing(5,6), archer, wizard })
