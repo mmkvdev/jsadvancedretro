8f3d6f3247c4289ded43764e535d72143c27f625
mmkvdev <madhuv299@gmail.com>
2025-01-19 22:11:15 +0530

    concept/types: updating pass by reference array with deep arrays at a specific index

--- types/passbyreference/mitigate/array.js
+++ types/passbyreference/mitigate/array.js
@@ -1,6 +1,6 @@
 // copying an array without modifying the original value
 
-const a = [1,2,3];
+const a = [1, 2, 3];
 
 // using concat method of an array
 const b = [].concat(a);
@@ -10,9 +10,10 @@ b.push(7);
 console.log({ a, b });
 
 // shallow copy of array
-const arrInit = [1,2,[3]];
+const arrInit = [1, 2, [3]];
 
 const shallowArrInit = [...arrInit];
+const jsonParsedArrInit = JSON.parse(JSON.stringify(arrInit));
 
 shallowArrInit[0] = 5;
 shallowArrInit[2][0] = 6;
