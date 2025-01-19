8e60899bbb1418598d2211f7151f8bde0f11e367
mmkvdev <madhuv299@gmail.com>
2025-01-19 17:24:10 +0530

    concept/types: array types

--- types/passbyreference/mitigate/array.js
+++ types/passbyreference/mitigate/array.js
@@ -8,3 +8,12 @@ const b = [].concat(a);
 b.push(7);
 
 console.log({ a, b });
+
+// shallow copy of array
+const arrInit = [1,2,[3]];
+
+const shallowArrInit = [...arrInit];
+
+shallowArrInit[0] = 5;
+shallowArrInit[2][0] = 6;
+console.log({ arrInit, shallowArrInit });
