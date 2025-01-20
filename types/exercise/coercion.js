66a08f4d1175f8d06cd8d1a61d07ef870091e7f4
mmkvdev <madhuv299@gmail.com>
2025-01-20 14:30:35 +0530

    concept/types: type coercion exercises

---
+++ types/exercise/coercion.js
@@ -0,0 +1,9 @@
+console.log(false == ""); // true
+console.log(false == []); // false - true
+console.log(false == {}); // false
+console.log("" == 0); // true
+console.log("" == []); // false
+console.log("" == {}); // false - true
+console.log(0 == []); // false
+console.log(0 == {}); // false
+console.log(0 == null); // false
