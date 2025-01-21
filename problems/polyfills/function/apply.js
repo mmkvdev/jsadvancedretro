189b0b104500d9be6138ceb32ac4024b6eed618d
mmkvdev <madhuv299@gmail.com>
2025-01-21 18:33:36 +0530

    problem/polyfills/function: polyfill for apply method

---
+++ problems/polyfills/function/apply.js
@@ -0,0 +1,44 @@
+/**
+ * Polyfill for Function.apply() method
+ * Syntax: Function.apply(thisArg, [arg1, arg2, ...])
+ */
+
+Function.prototype.customApply = function (thisArg, args) {
+  // ensure that `this` is a function
+  if (typeof this !== "function") {
+    throw new TypeError(`provided ${thisArg} is not a function`);
+  }
+
+  // if user didn't provided the `thisArg`, default to `globalThis`
+  thisArg = thisArg ?? globalThis;
+
+  // inject the contextual function to the provided `thisArg`
+  const uniqueFnKey = Symbol("fn");
+  thisArg[uniqueFnKey] = this;
+
+  const result = thisArg[uniqueFnKey](args);
+
+  delete thisArg[uniqueFnKey];
+
+  return result;
+};
+
+function isPerson(args) {
+  console.log(`person name is ${this.name}`, args);
+}
+
+// default case
+isPerson.customApply({
+  name: "mmk",
+});
+
+// when thisArg is passed as null
+isPerson.customApply();
+
+// when there are array of arguments
+isPerson.customApply(
+  {
+    name: "mmk",
+  },
+  [1, 2, 3]
+);
