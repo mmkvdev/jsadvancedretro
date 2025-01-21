246ff04ee45ad7d1fbd138cc7d710132f163b498
mmkvdev <madhuv299@gmail.com>
2025-01-21 18:40:04 +0530

    problem/polyfills/function: polyfill for call method and documentation updates

--- problems/polyfills/function/call.js
+++ problems/polyfills/function/call.js
@@ -12,7 +12,7 @@ Function.prototype.myCall = function (thisArg, ...args) {
   // if thisArg is not provided, default the value to `globalThis`
   thisArg = thisArg ?? globalThis;
 
-  // attach the contextual function to the provided and invoke it in the context of `thisArg`
+  // attach the contextual function to the provided `thisArg` and invoke it in the context of `thisArg`
   // creating a unique key because we don't know whether the custom function prop exists in the contextual function or not
   const uniqueFnKey = Symbol("fn");
   thisArg[uniqueFnKey] = this;
