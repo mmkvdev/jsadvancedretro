8d174f700331cf9d33fad652aa911f30b09a8cad
mmkvdev <madhuv299@gmail.com>
2025-01-27 12:55:00 +0530

    concept/fn: imperative vs declarative

---
+++ fp/index.js
@@ -0,0 +1,11 @@
+// imperative vs declarative
+
+// imperative
+console.log("-----imperative-----");
+for (let i = 0; i < 1000; i++) {
+  console.log(i);
+}
+
+// declarative
+console.log("-----declarative-----");
+[(1, 2, 3)].forEach((val) => console.log(val));
