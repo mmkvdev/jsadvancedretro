b3a9fec7a83f6a9dda01a820624beeaa09eeb3b1
mmkvdev <madhuv299@gmail.com>
2025-01-28 13:29:25 +0530

    concept/fp: currying exercise

---
+++ fp/currying/exercise.js
@@ -0,0 +1,36 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// curried sum
+function curry(f) {
+  return function (a) {
+    return function (b) {
+      return f(a, b);
+    };
+  };
+}
+
+let curriedSum = curry(sum);
+// console.log(curriedSum(2)(3));
+
+// sum function that can take many arguments
+function sum2(...args) {
+  return args.reduce((acc, val) => acc + val);
+}
+
+function curryAdv(f) {
+  return function curried(...args) {
+    if (args.length >= f.length) {
+      return f.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
+  };
+}
+
+let curriedSum2 = curryAdv(sum2);
+console.log(curriedSum2(1, 2, 3));
+// console.log(curriedSum2(1)(2)(3));
