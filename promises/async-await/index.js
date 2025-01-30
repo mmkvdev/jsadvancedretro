7142b9f0f8405f29d4cf48efaf1bd273275cf898
mmkvdev <madhuv299@gmail.com>
2025-01-30 22:33:51 +0530

    concept/promises: async await

---
+++ promises/async-await/index.js
@@ -0,0 +1,10 @@
+// async await
+import fetch from "node-fetch";
+
+const urls = [
+  "https://jsonplaceholdertypicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+async function getData() {}
