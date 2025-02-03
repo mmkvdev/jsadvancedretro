4ed637d1410a3d47baf35c7b3dab7f9b923221ee
mmkvdev <madhuv299@gmail.com>
2025-02-03 23:28:12 +0530

    concept/modules: exporting data at scripts

---
+++ modules/scripts/module.js
@@ -0,0 +1,18 @@
+//IIFE
+// Module Pattern
+
+var fightModule = (function () {
+  var harry = "Harry Potter";
+  var voldermort = "He who must not be named";
+
+  function fight(player1, player2) {
+    var attack1 = Math.floor(Math.random() * player1.length);
+    var attack2 = Math.floor(Math.random() * player2.length);
+
+    return attack1 > attack2 ? `${player1} wins` : `${player2} wins`;
+  }
+
+  return {
+    fight,
+  };
+})();
