ddc4fe66ba03a5cbb0acee36a2d35c04d72f99ca
mmkvdev <madhuv299@gmail.com>
2025-02-03 23:27:57 +0530

    concept/modules:scripts

---
+++ modules/scripts/index.js
@@ -0,0 +1,11 @@
+var harry = "Harry Potter";
+var voldermort = "He who must not be named";
+
+function fight(player1, player2) {
+  var attack1 = Math.floor(Math.random() * player1.length);
+  var attack2 = Math.floor(Math.random() * player2.length);
+
+  return attack1 > attack2 ? `${player1} wins` : `${player2} wins`;
+}
+
+fight(harry, voldermort);
